8d697a54f280135c75529ad88059231608156e24
Mydoobe Admin <mydoobe@gmail.com>
2025-03-11 15:18:29 +0100

    refactoring config scheme drizzle

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import { schema } from '../db/schema';
+import * as schema from '../db/schema'; // Importiere das gesamte Schema
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(pool, { schema }); // Übergebe das gesamte Schema
